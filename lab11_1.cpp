9c1a5d5c398724477ac62ffa853b8ee7e6a684cb
Tea067 <tawit_m@cmu.ac.th>
2025-12-30 15:00:31 +0700

    #

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string input;
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        getline(cin, input);
+    }
+
+    srand(time(0));
+    int n = rand() % 9;
+
+    cout << endl;
+    cout << "You will get " << grades[n]
+         << " in this 261102." << endl;
+
+    return 0;
+}
